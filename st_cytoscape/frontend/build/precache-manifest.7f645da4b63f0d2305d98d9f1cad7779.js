ff5b2dd5fb5941d6a939420f5d4f65955f7b998a
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2023-03-16 15:07:22 +0100

    Building

---
+++ st_cytoscape/frontend/build/precache-manifest.7f645da4b63f0d2305d98d9f1cad7779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333b86f35f7a7cbfdf99f6fab43cd039",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97a4c7de6242c262273d",
+    "url": "./static/js/2.c430191a.chunk.js"
+  },
+  {
+    "revision": "12ecc5f2f0d11f924dde9479673b3a5b",
+    "url": "./static/js/2.c430191a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee25918c71e453f91aab",
+    "url": "./static/js/main.6a3cf10d.chunk.js"
+  },
+  {
+    "revision": "4f647358ff6c1f15178c",
+    "url": "./static/js/runtime-main.db64ee98.js"
+  }
+]);
